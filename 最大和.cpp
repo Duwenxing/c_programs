7705b6f2f38ee2cc87e8176dbc7c38e5cd70b61b
Duwenxing <2273253997@qq.com>
2016-03-23 20:35:31 +0800

    最大和
    求一个数组中所有连续的子数组之和的最大值（考察动态规划）

---
+++ 最大和.cpp
@@ -0,0 +1,32 @@
+﻿#include<cstdio>
+#include<algorithm>
+#include<vector>
+using namespace std;
+int main()
+{
+	vector<int>array;
+	int n;
+	char flag=48;
+	while(flag!=10&&flag!=13)
+	{
+		scanf("%d",&n);
+		flag=getchar();
+		array.push_back(n);
+	}
+	int dp[array.size()];
+	dp[0]=array[0];
+	for(int i=1;i<array.size();i++)
+	{
+		dp[i]=max(array[i],dp[i-1]+array[i]);
+	}
+	int k=0;
+	for(int i=0;i<array.size();i++)
+	{
+		if(dp[i]>dp[k])
+		{
+			k=i;
+		}
+	}
+	printf("%d",dp[k]);
+	return 0;
+}
