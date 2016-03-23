474fb0f988537abd6cb861758dcaae2beec032a3
Duwenxing <2273253997@qq.com>
2016-03-23 20:33:04 +0800

    数组元素循环右移

---
+++ 数组元素循环右移.cpp
@@ -0,0 +1,25 @@
+﻿#include<cstdio>
+int main()
+{
+	int a[110];
+	int n,m,count=0;
+	scanf("%d%d",&n,&m);
+	m=m%n;
+	for(int i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(int i=n-m;i<n;i++)//先输出后m位
+	{
+		printf("%d",a[i]);
+		count++;
+		if(count<n) printf(" ");
+	}
+	for(int i=0;i<n-m;i++)//再输出前n-m位
+	{
+		printf("%d",a[i]);
+		count++;
+		if(count<n) printf(" ");
+	}
+	return 0;
+}
